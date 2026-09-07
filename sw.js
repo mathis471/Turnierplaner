74c7483df4af454da9c1c780601e18d37d3ad770
mathis471 <mathisborrink47@gmail.com>
2026-09-07 22:19:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE="dart-turnier-v5-modern-bg-2";
+const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./dart-background.jpg"];
+self.addEventListener("install",event=>{
+ event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+ event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+ if(event.request.method!=="GET") return;
+ event.respondWith(
+  fetch(event.request).then(response=>{
+   const copy=response.clone();
+   caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+   return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
+ );
+});
